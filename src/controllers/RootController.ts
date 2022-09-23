61604fa39e8cefd790c750ef8af5c82e74a3be05
daberry_wh <daberry@williamhill.us>
2022-09-23 12:25:17 -0700

    express backend with cutom ts library

---
+++ src/controllers/RootController.ts
@@ -0,0 +1,43 @@
+import { controller, get, use } from "./decorators";
+
+import { Request, Response, NextFunction } from "express";
+import { get, use, controller } from "./decorators";
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send("Not permitted");
+}
+
+@controller("")
+class RouteController {
+  @get("/")
+  getRoute(req: Request, res: Response) {
+    console.log("hi");
+    if (req.session && req.session.loggedIn) {
+      res.send(`
+        <div>
+          <div>You are logged in</div>
+          <a href="/auth/logout">Logout</a>
+        </div>
+      `);
+    } else {
+      res.send(`
+        <div>
+          <div>You are not logged in</div>
+          <a href="/auth/login">Login</a>
+        </div>
+      `);
+    }
+  }
+
+  @get("/protected")
+  @use(requireAuth)
+  getProtected(req: Request, res: Response) {
+    res.send("Welcome to protected route, logged in user");
+  }
+}
---
+++ src/controllers/decorators/controller.ts
@@ -0,0 +1,60 @@
+import "reflect-metadata";
+import { AppRouter } from "../../AppRouter";
+import { Methods } from "./Methods";
+import { MetadataKeys } from "./MetadataKeys";
+import { NextFunction, RequestHandler, Request, Response } from "express";
+
+function bodyValidator(keys: string): RequestHandler {
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      return res.status(422).send("Invalid request");
+    }
+
+    for (let key of keys) {
+      if (!req.body[key]) {
+        return res.status(422).send(`Missing property ${key}`);
+      }
+    }
+
+    next();
+  };
+}
+
+export function controller(routePrefix: string) {
+  return function (target: Function) {
+    const router = AppRouter.getInstance();
+
+    for (let key in target.prototype) {
+      const routeHandler = target.prototype[key];
+      const path = Reflect.getMetadata(
+        MetadataKeys.path,
+        target.prototype,
+        key
+      );
+      const middlewares =
+        Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) ||
+        [];
+
+      const method: Methods = Reflect.getMetadata(
+        MetadataKeys.method,
+        target.prototype,
+        key
+      );
+
+      const requiredBodyProps =
+        Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) ||
+        [];
+
+      const validator = bodyValidator(requiredBodyProps);
+
+      if (path) {
+        router[method](
+          `${routePrefix}${path}`,
+          ...middlewares,
+          validator,
+          routeHandler
+        );
+      }
+    }
+  };
+}
